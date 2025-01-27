f67699635ad892efaf4b16631548cfef00c5fa68
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-01-27 09:27:13 +0200

    working on stylying

--- src/components/NarrativeWindow.vue
+++ src/components/NarrativeWindow.vue
@@ -1,18 +1,21 @@
 <script setup>
-import { useCounterStore } from '../stores/counter'; 
-const counterStore = useCounterStore();
-const increment = counterStore.increment;
-const decrement = counterStore.decrement;
-const reset = counterStore.reset;
+// import { useCounterStore } from '../stores/counter'; 
+// const counterStore = useCounterStore();
+// const increment = counterStore.increment;
+// const decrement = counterStore.decrement;
+// const reset = counterStore.reset;
 
 
 </script>
 
 <template>
       <div>
-    <p>Counter: {{ counterStore.count }}</p>
+    <!-- <p>Counter: {{ counterStore.count }}</p>
     <button @click="increment">Increment</button>
     <button @click="decrement">Decrement</button>
-    <button @click="reset">Reset</button>
+    <button @click="reset">Reset</button> -->
+
+    <h1>question</h1>
+    <h2>answer field</h2>
   </div>
 </template>
