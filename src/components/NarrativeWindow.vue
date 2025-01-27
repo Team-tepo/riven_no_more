fdd7e81e1b393dc3ca2b95b34af868f96920a01c
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-01-27 10:51:05 +0200

    dark mode

--- src/components/NarrativeWindow.vue
+++ src/components/NarrativeWindow.vue
@@ -2,7 +2,10 @@
 </script>
 
 <template>
-    
-    <button class="btn btn-primary">hi
-    </button>
+    <div>
+  <b-button>Button</b-button>
+  <b-button variant="danger">Button</b-button>
+  <b-button variant="success">Button</b-button>
+  <b-button variant="outline-primary">Button</b-button>
+</div>
 </template>
