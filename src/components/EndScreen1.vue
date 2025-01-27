d8b08d541b67257e2248479dc571a77d57711207
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-01-27 08:47:34 +0200

    1. added end screen

---
+++ src/components/EndScreen1.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: false,
+  },
+})
+</script>
+
+<template>
+    <h1>hello from end screen</h1>
+</template>
+
+<style scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup>
 import NarrativeWindow from '../components/NarrativeWindow.vue'
+// import EndScreen1 from '../components/EndScreen1.vue'
 
 </script>
 
 <template>
     <NarrativeWindow/>
+    <!-- <EndScreen1/> -->
 </template>
