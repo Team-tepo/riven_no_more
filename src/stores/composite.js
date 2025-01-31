721ba7752fc8fadc69e03c31c568b29e7ee77be3
Abdool-Fima <abdool@fima.ai>
2025-01-31 10:30:24 +0200

    need to fix id

--- src/stores/composite.js
+++ src/stores/composite.js
@@ -1,10 +1,10 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
-import  narr from '../stores/narrative.json' 
+import  narr from '../stores/output.json' 
 
 
 export const UseCompositeFunc = defineStore('narrative', () => {
-    const position = ref(narr.findIndex(item=>item.node_id==1))
+    const position = ref(narr.findIndex(item=>item.node_id==29))
     function step(idnum){
       position.value= narr.findIndex(item=>item.node_id==idnum)
       console.log( 'should go to ', idnum, 'index: ', position.value)
